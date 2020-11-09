fa2e7ddb45f706a02b1805c6f6b0b349657614a7
wellrafi <wellrafiuniverse@gmail.com>
2020-11-09 18:04:11 +0700

    fixing home page

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
         $(this).toggleClass('active')
         $('.mobile-menu').toggleClass('active')
     })
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        loop:true,
+        dots: false,
+        loop: true,
+        nav: true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        navText: ["<img src='/assets/icons/arrow_left.svg'>","<img src='/assets/icons/arrow_right.svg'>"],
+        responsive:{
+            1000:{
+                items:1
+            }
+        }
+    })
 })
